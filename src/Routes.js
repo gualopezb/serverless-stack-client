e5a75f0e87da88c3addf1aadb51b073233401658
Gustavo López <gualopezb@gmail.com>
2020-07-26 14:22:02 -0500

    Add login form

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,16 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
       </Route>
       {/* Finally, catch all unmatched routes */}
       <Route>
