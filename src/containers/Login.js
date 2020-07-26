4c60fcec6256bf1b7a4a2f7cc965dc3d60ffef8f
Gustavo López <gualopezb@gmail.com>
2020-07-26 15:22:55 -0500

    Show spinner while logging in

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { Auth } from "aws-amplify";
 
 import { useAppContext } from "../libs/contextLib";
+import { onError } from "../libs/errorLib";
+import LoaderButton from "../components/LoaderButton";
 
 import "./Login.css";
 
@@ -12,6 +14,7 @@ export default function Login() {
   const { userHasAuthenticated } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -20,12 +23,15 @@ export default function Login() {
   async function handleSubmit(event) {
     event.preventDefault();
 
+    setIsLoading(true);
+
     try {
       await Auth.signIn(email, password);
       userHasAuthenticated(true);
       history.push("/");
     } catch (e) {
-      alert(e.message);
+      onError(e);
+      setIsLoading(false);
     }
   }
 
@@ -49,9 +55,15 @@ export default function Login() {
             type="password"
           />
         </FormGroup>
-        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </form>
     </div>
   );
