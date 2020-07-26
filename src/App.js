9404e864efc8c9dd8c85e4f2a0d47e14c9557c0b
Gustavo López <gualopezb@gmail.com>
2020-07-26 14:34:40 -0500

    Store the session in the context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 import Routes from "./Routes";
+import { AppContext } from "./libs/contextLib";
 
 import "./App.css";
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  function handleLogout() {
+    userHasAuthenticated(false);
+  }
+
   return (
     <div className="App container">
       <Navbar fluid collapseOnSelect>
@@ -19,16 +26,24 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            {isAuthenticated ? (
+              <NavItem onClick={handleLogout}>Logout</NavItem>
+            ) : (
+              <>
+                <LinkContainer to="/signup">
+                  <NavItem>Signup</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <NavItem>Login</NavItem>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { Auth } from "aws-amplify";
 
+import { useAppContext } from "../libs/contextLib";
+
 import "./Login.css";
 
 export default function Login() {
+  const { userHasAuthenticated } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -17,7 +20,7 @@ export default function Login() {
 
     try {
       await Auth.signIn(email, password);
-      alert("Logged in");
+      userHasAuthenticated(true);
     } catch (e) {
       alert(e.message);
     }
