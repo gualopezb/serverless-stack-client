34c38ce15a18a580be6e824cb7a52677c7c0ed3d
Gustavo López <gualopezb@gmail.com>
2020-07-26 13:57:07 -0500

    Add navbar and home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+
+import Routes from "./Routes";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./containers/Home";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  );
+}
