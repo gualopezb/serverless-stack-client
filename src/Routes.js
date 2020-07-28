5a8adaf056509c8b179229c5926502851f999dab
Gustavo López <gualopezb@gmail.com>
2020-07-28 17:45:27 -0500

    Add ability of viewing and editing a note

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default function Routes() {
   return (
@@ -21,6 +22,9 @@ export default function Routes() {
       </Route>
       <Route exact path="/notes/new">
         <NewNote />
+      </Route>
+      <Route exact path="/notes/:id">
+        <Notes />
       </Route>
       {/* Finally, catch all unmatched routes */}
       <Route>
