ddbaaebb8f2a74e654f49e4497bee597be1f95da
Gustavo López <gualopezb@gmail.com>
2020-07-31 16:33:12 -0500

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 import { API } from "aws-amplify";
 
 import { useAppContext } from "../libs/contextLib";
@@ -61,6 +62,14 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
